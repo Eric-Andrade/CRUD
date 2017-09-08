1974da21a9ac5984fa550e86b74721124c4889a4
Eric-Andrade <erictorresandrade.1@gmail.com>
2017-09-08 15:40:33 -0500

    Pre-getClients

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { ActivityIndicator } from 'react-native';
 import styled from 'styled-components/native';
+import { getClients } from './src/util/constants'
 
 const Root = styled.View`
   flex: 1;
@@ -13,10 +15,35 @@ const T = styled.Text`
 `;
 
 export default class App extends React.Component {
+  static defaultProps = {
+    getClients
+  }
+
+  state = {
+    loading: false,
+    clients: []
+  }
+
+  async componentDidMount(){
+    this.setState({loading: true});
+    const data = await this.props.getClients();
+    setTimeout(() => this.setState({loading: false, clients: data.clients}),2000);
+  }
+
   render() {
+    if (this.state.loading) {
+      return (
+        <Root>
+            <ActivityIndicator size="large" color="#333" />
+        </Root>
+      )
+    }
     return (
     <Root>
       <T>Welcome CRUD</T>
+      {this.state.clients.map((client, i) => (
+        <T key={i}>{client.CNOMBRE}</T>
+      ))}
     </Root>
     );
   }
--- src/util/constants.js
+++ src/util/constants.js
@@ -1,1 +1,7 @@
 const URL = 'http://itecordurango.com/subdominios/api_kityplancho/api/'
+
+export const getClients = () => {
+  return fetch(`${URL}clientes`)
+    .then(response => response.json())
+   
+}
